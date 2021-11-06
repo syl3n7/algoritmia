101d68f35b0feeffa28b930b3c7f0c0d9f901ac4
syl3n7 <claudiosilva@protonmail.com>
2021-11-06 13:03:33 +0000

    update

---
+++ CensorWord/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace CensorWord
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string finalsentence;
+            string censoredword;
+            string censored = "";
+            string sentence;
+            
+            
+            Console.WriteLine("Please introduze the forbidden word: ");
+            censoredword = Console.ReadLine();
+            
+            Console.WriteLine("Please enter the sentence: ");
+            sentence = Console.ReadLine();
+                
+            if (sentence.Contains(censoredword))
+            {
+                for (int i = 0; i < censoredword.Length; i++)
+                {
+                    censored += "*";
+                }
+                finalsentence = sentence.Replace(censoredword, censored);
+                Console.WriteLine($"The censored sentenced is:\n{finalsentence}");
+            }
+            else
+            {
+                Console.WriteLine("The sentence is clear!");
+            }
+        }
+    }
+}
