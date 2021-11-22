30c4dcafdec24ec9bacbb89d3a9f01d253da259d
syl3n7 <claudiosilva@protonmail.com>
2021-11-22 15:23:48 +0000

    Adição 2 projetos
    Miniteste e Ex de reviso para prox teste pratico

---
+++ Ex5 - Revisao/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+namespace Ex5___Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Introduza uma frase com amis de duas palavras");
+            string original, first, last;
+            string space = " ";
+            original = Console.ReadLine();
+
+            
+            for (int i = 0; i < original.Length; i++)
+            {
+
+                if (original.Contains(space))
+                {
+                    
+                }
+                first = original.Substring(i);
+
+            }
+/*
+if (sentence.Contains(censoredword))
+            {
+                for (int i = 0; i < original.Length; i++)
+                {
+                     += "*";
+                }
+                finalsentence = sentence.Replace(censoredword, censored);
+                Console.WriteLine($"The censored sentence is:\n{finalsentence}");
+            }        
+*/
+        }
+    }
+}
+
+
---
+++ MiniTeste/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Linq;
+
+namespace Numeros_Teste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many numbers?");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if(n > 2){
+                int[] a = new int[n];
+                int menor = 0;
+                for (int i = 0; i < n; i++)
+                {
+                    Console.WriteLine($"Number: {i}");
+                    a[i] = Convert.ToInt32(Console.ReadLine());
+                    if (i == 0 || menor > a[i])
+                        menor = a[i];
+                }
+                Console.WriteLine($"The Lowest number is: {menor}");
+            } else {
+                Console.WriteLine("Introduza numero superior a 2");
+            }
+       }
+    }
+}
