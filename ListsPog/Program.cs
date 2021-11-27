87d9fa21888c79936a594f47f68699d186308f0a
syl3n7 <claudiosilva@protonmail.com>
2021-11-27 00:56:08 +0000

    idk - probablu should check if finished

--- ListsPog/Program.cs
+++ ListsPog/Program.cs
@@ -10,20 +10,34 @@ namespace ListsPog
         {
 
             palavrasuser();
+            criararray();
 
+        }
+        
+        static 
+        
+        static void criararray()
+        {
+            List<int> numberlist = new List<int>(new int[] { 65, 47, 100, 3, 19 });
         }
         
         static void palavrasuser()
         {
             
             //create an array with 10 words introduced by the user
+            Console.WriteLine("Introduza 10 palavras :)\n");
             List<String> words = new List<string>(10);
             for (int i = 0; i < words.Capacity; i++)
             {
                 words.Add(Console.ReadLine());
             }
+
+            for (int i = 0; i < words.Count; i++)
+            {
+                words.Reverse();
+                Console.WriteLine($"Palavra {i}: {words[i]}");
+            }
         }
-        
-        
+
     }
 }
