24d0448e29653445dd854767770a379eb60247e0
syl3nt <claudiosilva@protonmail.com>
2021-12-20 23:41:11 +0000

    pog

---
+++ Ex5 - Listas/ConsoleApp1/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //generate array of random numbers
+            int[] array = new int[10];
+            Random rnd = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = rnd.Next(1, 100);
+            }
+            
+            //print array
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+            Console.Clear();
+            Console.WriteLine("im going to sort the array now");
+            Console.ReadKey();
+            
+            //bubble sort the array
+            for (int i = 0; i < array.Length; i++)
+            {
+                for (int j = 0; j < array.Length - 1; j++)
+                {
+                    if (array[j] > array[j + 1])
+                    {
+                        int temp = array[j];
+                        array[j] = array[j + 1];
+                        array[j + 1] = temp;
+                    }
+                }
+            }
+            Console.ReadKey();
+            //print the sorted array
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+
+        }
+    }
+}
---
+++ Ex5 - Listas/Testing Copilot and Tabnine/ConsoleApp1/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+
+var random = new Random();
+var array = new Array(100);
+for (var i = 0; i < array.length; i++) {
+    array[i] = random.next();
+}
+
+
+
+
